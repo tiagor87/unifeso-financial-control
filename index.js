005964723fe684b3ee5fd6e7ad67791a534cba99
Patrícia Fontenelle <patricia.fontenelle@hotmail.com>
2020-10-09 15:18:07 -0300

    Resolução da Av1

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 
 // CREATE
 app.post("/", async (request, response) => {
+  debugger;
   const jsonContent = request.body;
   // User -> objeto responsável por acessar o banco de dados.
   const user = await User.create(jsonContent);
@@ -31,36 +32,48 @@ app.post("/", async (request, response) => {
 
 // READ
 app.get("/:id", (request, response) => {
-  const obj = dictionary[request.params.id];
-  if (!obj) {
-    return response.status(404).send({
-      message: "Not found"
-    });
-  }
-  response.status(200).send(obj);
+  User.findById(request.params.id, (err, user) => {
+    if(err) {
+      return response.status(404).send({
+        message: "O usuário não foi encontrado."
+      })
+    } else {
+      return response.status(201).send(user);
+    }
+  });
 });
 
 // UPDATE
 app.put("/:id", (request, response) => {
-  const obj = request.body;
-  if (!dictionary[request.params.id]) {
-    return response.status(404).send({
-      message: "Usuário não encontrado."
-    });
-  }
-  dictionary[request.params.id] = obj;
-  response.status(200).send(obj);
+  const updatedObj = request.body;
+
+  User.findByIdAndUpdate(request.params.id, updatedObj, (err, user) => {
+    if(err) {
+      return response.status(404).send({
+        message: "Usuário não encontrado."
+      }) 
+    } else {
+      return response.status(201).send({
+        message: "Cadastro alterado com sucesso.",
+        user: user
+      })
+    }
+  });
 });
 
 // DELETE
 app.delete("/:id", (request, response) => {
-  if (!dictionary[request.params.id]) {
-    return response.status(404).send({
-      message: "Usuário não encontrado."
-    });
-  }
-  dictionary[request.params.id] = null;
-  response.status(204).end();
+  User.findByIdAndDelete(request.params.id, (err, user) => {
+    if(err) {
+      return response.status(404).send({
+        message: "O usuário não foi encontrado."
+      })
+    } else {
+      return response.status(201).send({
+        message: "Cadastro excluído com sucesso!"
+      });
+    }
+  });
 });
 
 const port = 8090;
