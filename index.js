1a087627c34cd49faf82055db5552e457d575f8d
Patrícia Fontenelle <patricia.fontenelle@hotmail.com>
2020-11-19 16:32:37 -0300

    Resolvendo conflitos de merge

--- index.js
+++ index.js
@@ -20,65 +20,8 @@ const app = express();
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-<<<<<<< HEAD
-// CREATE
-app.post("/", async (request, response) => {
-  debugger;
-  const jsonContent = request.body;
-  // User -> objeto responsável por acessar o banco de dados.
-  const user = await User.create(jsonContent);
-  response.status(201).send(user);
-});
-
-// READ
-app.get("/:id", (request, response) => {
-  User.findById(request.params.id, (err, user) => {
-    if(err) {
-      return response.status(404).send({
-        message: "O usuário não foi encontrado."
-      })
-    } else {
-      return response.status(201).send(user);
-    }
-  });
-});
-
-// UPDATE
-app.put("/:id", (request, response) => {
-  const updatedObj = request.body;
-
-  User.findByIdAndUpdate(request.params.id, updatedObj, (err, user) => {
-    if(err) {
-      return response.status(404).send({
-        message: "Usuário não encontrado."
-      }) 
-    } else {
-      return response.status(201).send({
-        message: "Cadastro alterado com sucesso.",
-        user: user
-      })
-    }
-  });
-});
-
-// DELETE
-app.delete("/:id", (request, response) => {
-  User.findByIdAndDelete(request.params.id, (err, user) => {
-    if(err) {
-      return response.status(404).send({
-        message: "O usuário não foi encontrado."
-      })
-    } else {
-      return response.status(201).send({
-        message: "Cadastro excluído com sucesso!"
-      });
-    }
-  });
-});
-=======
 app.use("/users", userRouter);
 app.use(ErrorHandlerMiddleware);
->>>>>>> upstream/master
 
 const port = 8090;
 app.listen(port, () => console.log(`Rodando em localhost:${port}`));
