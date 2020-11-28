df98f89fb96a0d66957d7d89b8732dffabdf1b67
NathanZimbrao <64225559+NathanZimbrao@users.noreply.github.com>
2020-11-28 13:58:35 -0300

    Mudanças para o trabalho da AV2
    tem algumas partes que tive que comentar porque estava conflitando tipo o PUT para trocar senha.

--- routers/user_router.js
+++ routers/user_router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { UserService } = require("../services");
 const asyncHandler = require("express-async-handler");
+const { request, response } = require("express");
+const { PreconditionFailedError } = require("../errors");
 
 const userService = new UserService();
 const userRouter = express.Router();
@@ -36,13 +38,16 @@ userRouter.get(
 
 // UPDATE
 // /users/<id>
+/*
 userRouter.put(
   "/:id",
   asyncHandler(async (request, response) => {
+    
+    console.log("passou aqui sim!");
     const user = await userService.updateAsync(request.params.id, request.body);
     response.status(200).send(user);
   })
-);
+);*/
 
 // DELETE
 // /users/<id>
@@ -54,4 +59,45 @@ userRouter.delete(
   })
 );
 
+//users/login
+userRouter.post(//login teve q ser post pq só assim consigo passar um json no body com o username e a senha
+  "/login",
+    asyncHandler(async (request, response) => {
+    const user = await userService.login(request.body);
+    response.status(200).send(user);
+  })
+);
+
+//users/singin
+userRouter.post(
+  "/singin",
+  asyncHandler(async (request, response) => {
+    const createUser = request.body;
+    try {
+      const user = await userService.singin(createUser);
+      response.status(201).send(user);
+    } catch (error) {
+      if(error instanceof PreconditionFailedError) {
+        response.status(400).send({});
+      }
+      else {
+        response.status(406).send(error);
+      }
+    }
+  })
+);
+//users/update_password
+userRouter.put(
+  "/update_password",
+  asyncHandler(async (request, response) => {
+    const createUser = request.body;
+    try {
+      const user = await userService.senhaPORA(createUser.username, createUser.password);
+      response.status(201).send(user);
+    } catch (error) {
+      //TODO: tratar os erros que podem retornar
+      response.status(406).send(error);//sem tempo pra tratar esse aqui antes da entrega
+    }
+  })
+);
 module.exports = userRouter;
